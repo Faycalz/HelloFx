ce1cd5ff96254132342b7480af9a912be0e609d7
Faycalz <zetoutoufaycal@gmail.com>
2021-04-20 11:39:24 +0100

    Third commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
 		scene.getStylesheets().add(css);
 		Image icon = new Image("dd.png");
 		stage.getIcons().add(icon);
-		stage.setTitle("HelloFxx");
+		stage.setTitle("Ghozlane");
 		
 		
 		
